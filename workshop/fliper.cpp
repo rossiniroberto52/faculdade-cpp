1a80cdf0e7203208ba2116c3c8d18ba5734b87cb
rossini135 <toquehuk@gmail.com>
2025-03-29 21:49:53 -0300

    first commit

---
+++ workshop/fliper.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int v;
+    int t;
+
+    cin>>v;
+    cin>>t;
+
+    double res = (v*t);
+    cout << res;
+
+    return 0;
+}
---
+++ workshop/maiorNum.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int num;
+    int nums[];
+    for(int i = 0; if(num >= 0); i++){
+        cin>>num;
+        nums[i]=num;
+    }
+
+    return 0;
+}
