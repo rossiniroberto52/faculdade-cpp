c4a36aae33251f50186d46c21913c2260662c53a
rossini135 <toquehuk@gmail.com>
2025-03-30 09:36:11 -0300

    teste1 up

--- testes/teste1.cpp
+++ testes/teste1.cpp
@@ -1,8 +1,23 @@
 #include<iostream>
-using namespace std;
+#include<bits/stdc++.h>
 
+using namespace std;
+// currency converter
 int main(){
-    
+    int choice;
+    cout << "1) USD \n2) EUR \nchoice: ";
+    cin >> choice;
+    double amount;
+    cout << "How much money to convert(BRL)? R$";
+    cin >> amount;
+
+    if(choice == 1){
+        double USD = amount/5.74;
+        cout << "You have: " << USD << "USD" << endl;
+    }else if(choice == 2){
+        double EUR = amount/6.33;
+        cout << "You have: " << EUR << "EUR" << endl;
+    }
 
     return 0;
 }
